524226b16e5568c29888026dfe8077797c300674
angelorobson <helloween123>
2021-03-18 15:09:53 -0300

    Implemented ok http log interceptor

--- app/src/main/java/com/angelorobson/monitorerrorapp/di/ApplicationModules.kt
+++ app/src/main/java/com/angelorobson/monitorerrorapp/di/ApplicationModules.kt
@@ -2,6 +2,7 @@ package com.angelorobson.monitorerrorapp.di
 
 import android.text.format.DateUtils
 import com.angelorobson.monitorerrorapp.BuildConfig.BASE_URL_API
+import com.angelorobson.monitorerrorapp.BuildConfig.BUILD_TYPE
 import com.angelorobson.monitorerrorapp.R
 import com.angelorobson.monitorerrorapp.utils.ActivityService
 import com.angelorobson.monitorerrorapp.utils.BaseWebService
@@ -9,6 +10,7 @@ import com.angelorobson.monitorerrorapp.utils.NavigationNavigator
 import com.angelorobson.monitorerrorapp.utils.customDateAdapter
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import java.util.concurrent.TimeUnit
 
@@ -18,6 +20,7 @@ val applicationModules = module(override = true) {
     single {
         BaseWebService(
             baseUrl = BASE_URL_API,
+            buildType = BUILD_TYPE,
             connectTimeout = DateUtils.MINUTE_IN_MILLIS / HALF,
             readTimeout = DateUtils.MINUTE_IN_MILLIS / HALF,
             writeTimeout = DateUtils.MINUTE_IN_MILLIS / HALF,
@@ -35,10 +38,16 @@ val applicationModules = module(override = true) {
     single {
         val baseWebService = get<BaseWebService>()
 
-        OkHttpClient.Builder()
+        val okHttpClient = OkHttpClient.Builder()
             .connectTimeout(baseWebService.connectTimeout, TimeUnit.MILLISECONDS)
             .readTimeout(baseWebService.readTimeout, TimeUnit.MILLISECONDS)
-            .writeTimeout(baseWebService.writeTimeout, TimeUnit.MILLISECONDS).build()
+            .writeTimeout(baseWebService.writeTimeout, TimeUnit.MILLISECONDS)
+
+        if (baseWebService.buildType == "debug") {
+            okHttpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        }
+
+        okHttpClient.build()
     }
 
     single {
--- app/src/main/java/com/angelorobson/monitorerrorapp/utils/BaseWebService.kt
+++ app/src/main/java/com/angelorobson/monitorerrorapp/utils/BaseWebService.kt
@@ -2,6 +2,7 @@ package com.angelorobson.monitorerrorapp.utils
 
 data class BaseWebService(
     val baseUrl: String,
+    val buildType: String,
     val connectTimeout: Long,
     val readTimeout: Long,
     val writeTimeout: Long,
